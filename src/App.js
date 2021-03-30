6555734f1b73a49645fd7352d4b8c1a27d5d2f01
hypocrisy <conaniskenan@gmail.com>
2021-03-30 21:07:20 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent, Fragment } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends PureComponent {
+	constructor(props) {
+		super(props)
+		this.state = {
+			inputValue: '',
+			list: [],
+		}
+	}
+
+	render() {
+		let { inputValue, list } = this.state
+		return (
+			<Fragment>
+				<div>
+					<input
+						type="text"
+						value={inputValue}
+						onChange={e => {
+							this.handleChange(e)
+						}}
+					/>
+					<button
+						onClick={e => {
+							this.handleClick(e)
+						}}
+					>
+						添加
+					</button>
+				</div>
+				<ul>
+					{list.map((item, index) => {
+						return (
+							<Fragment>
+								<div>
+									<li key={index} style={{ display: 'inline-block' }}>
+										{item}
+									</li>
+									<button
+										onClick={e => {
+											this.handleItemDelete(index)
+										}}
+									>
+										删除
+									</button>
+								</div>
+							</Fragment>
+						)
+					})}
+				</ul>
+			</Fragment>
+		)
+	}
+	handleClick(e) {
+		let value = this.state.inputValue
+		if (value.trim().length === 0) {
+			this.setState({
+				inputValue: '',
+			})
+		} else {
+			this.setState({
+				list: [...this.state.list, this.state.inputValue],
+				inputValue: '',
+			})
+		}
+	}
+	handleChange(e) {
+		this.setState({
+			inputValue: e.target.value,
+		})
+	}
+	handleItemDelete(index) {
+		const list = [...this.state.list]
+		list.splice(index, 1)
+		this.setState({
+			list,
+		})
+	}
 }
-
-export default App;
